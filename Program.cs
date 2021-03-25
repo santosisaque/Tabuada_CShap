7344f12789af2d61c2f42d32dbbc3814985d2376
santosisaque <67133116+santosisaque@users.noreply.github.com>
2021-03-24 23:13:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EX_4n_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int valor1, valor2, atvextra, soma;
+            double media;
+            Console.Write("Escolha a tabuada: ");
+            valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEscolha o limite da tabuada: ");
+            valor2 = Convert.ToInt32(Console.ReadLine());
+
+            for(i=1; i<=valor2; i++)
+            {
+                Console.WriteLine("{0} x {1}={2}", valor1.ToString(), i.ToString(), (valor1 * i).ToString());
+            }
+
+        
+            
+        }
+    }
+}
